d396039e8bfa8014c1b217f76e78f749ee242487
angiemcgraw <mangie@pdx.edu>
2020-10-08 19:01:50 -0700

    input page skeleton w/ new design

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,8 +1,5 @@
 import React, {useState, useEffect} from 'react'
 import './Info.css'
-import RoomIcon from '@material-ui/icons/Room';
-import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
-import CommuteIcon from '@material-ui/icons/Commute';
 
 function Info() {
     const [address, setAddress] = useState();
@@ -27,43 +24,28 @@ function Info() {
             </div>
 
             <div className = "location-section">
-                <h1 className = "title">patient location:</h1>
+                <h1 className = "title">Patient Location:</h1>
                 <br />
                 <div className = "patient-location-section">
-                    <RoomIcon 
-                        className = "map-icon"
-                        style = {{color:'red'}}
-                    />
-                    <div className = "input-section">
-                        <label>Coordinates: </label>
-                        <br />
-                        <input placeholder = "Coordinates" />
-                        <hr />
-
-                        <br />
-
-                        <label>Address: </label> <br />
-                        <input placeholder = "Enter address here" value={address}
-                        onChange={handleChange} type="text"
-                        />
-                        {/* <input type="submit" value={address}
-                        onChange={handleChange} /> */}
-                        <hr />
-                    </div>
+                    <img src = "../images/map_example.jpg" alt = ""/>
                 </div>
+                <br />
+                <br />
+
+                <div className = "manual-address-input">
+                    <button className = "manual-address-btn">Manual Address Input</button>
+                </div>
+
             </div>
 
             <br />
             <br />
             <br />
+
             <div className = "hospital-section">
                 <h1 className = "title">Hospital Selection:</h1>
                 <br />
                 <div className = "hospital-selection-section">
-                    <LocalHospitalIcon 
-                        className = "hospital-icon"
-                        style = {{color:'blue'}}
-                    />
                     <div className = "drop-down-section">
                         <form>
                             <select>
@@ -77,26 +59,67 @@ function Info() {
 
             <br />
             <br />
-            <br />
+
             <div className = "helicopter-section">
-                <h1 className = "title">Helicopter Landing Selection:</h1>
+                <h1 className = "title">Helicopter Site:</h1>
                 <br />
-                <div className = "helicopter-selection-section">
-                    <CommuteIcon 
-                        className = "helicopter-icon"
-                        style = {{color:'green'}}
-                    />
+                <div className = "helicopter-section">
                     <div className = "drop-down-section">
                         <form>
                             <select>
-                                <option value = "ex3">Landing Site 1</option>
-                                <option value = "ex4">Landing Site 2</option>
+                                <option value = "ex1">Site 1</option>
+                                <option value = "ex2">Site 2</option>
                             </select>
                         </form>
                     </div>
                 </div>
             </div>
 
+            <br />
+            <br />
+
+            <div className = "loading-time-section">
+                <h1 className = "title">Estimated Patient Loading Time</h1>
+                <br />
+                <div className = "drop-down-section">
+                        <form>
+                            <select>
+                                <option value = "ex1">5 minutes</option>
+                                <option value = "ex2">10 minutes</option>
+                                <option value = "ex2">15 minutes</option>
+                            </select>
+                        </form>
+                </div>
+            </div>
+
+            <br />
+            <br />
+
+            <div className = "estimated-heli-time-section">
+                <h1 className = "title">Estimated Time to Helicopter</h1>
+                <br />
+                <div className = "helicopter-section">
+                    <div className = "input-section">
+                        <input placeholder = "(leave blank if none)" value={address}
+                        onChange={handleChange} type="text"
+                        />
+                        {/* <input type="submit" value={address}
+                        onChange={handleChange} /> */}
+                        <hr />
+                    </div>
+                </div>
+            </div>
+
+            <br />
+            <br />
+
+            <div className = "calculate-section">
+                <br />
+                <br />
+                <div className = "calculate-section">
+                        <button className = "calculate-btn">Calculate</button>
+                </div>
+            </div>
 
         </div>
     )
