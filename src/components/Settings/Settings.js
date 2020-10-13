9888811062f2e2cca4b963373140f7274d8b2b0f
angiemcgraw <mangie@pdx.edu>
2020-10-12 19:45:38 -0700

    time estimations section

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -29,13 +29,13 @@ function Settings() {
                 <br />
                 <div className="horizontal"></div>
                 <br />
-                <div className="settings-inputs-bottom">
+                <div className="settings-inputs-middle">
                     <h1 className="title">add Helicopter dispatch locations:</h1>
-                    <label className="settings-label-bottom">Helipad name:</label>
+                    <label className="settings-label-middle">Helipad name:</label>
                     <input placeholder="Enter name here" />
 
-                    <label className="settings-label-bottom">Helipad Address:</label>
-                    <input placeholder="Enter name here" />
+                    <label className="settings-label-middle">Helipad Address:</label>
+                    <input placeholder="Enter address here" />
 
                     <div className="btn-div">
                     <div></div>
@@ -49,7 +49,39 @@ function Settings() {
                     <br />
                 </div>
 
-                <h1 className="title">time estimations:</h1>
+                <div className="settings-inputs-bottom">
+                    <h1 className="title">Time Estimations:</h1>
+                    <label className="settings-label-bottom">Ambulance arrives at hospital 
+                        to patient reaches ER:</label>
+                    <input placeholder="Enter estimation here" />
+
+                    <label className="settings-label-bottom">Helicopter arrives at hospital
+                        to patient reaches ER:</label>
+                    <input placeholder="Enter estimation here" />
+
+                    <label className="settings-label-bottom">Time it takes helicopter to get 
+                        ready and takeoff from helipad:</label>
+                    <input placeholder="Enter estimation here" />
+
+                    <label className="settings-label-bottom">Time to meet helicopter where
+                        it lands:</label>
+                    <input placeholder="Enter estimation here" />
+
+                    <label className="settings-label-bottom">Time to get patient ready 
+                        for departure:</label>
+                    <input placeholder="Enter estimation here" />
+
+                    <div className="btn-div">
+                    <div></div>
+                    <button
+                        className="submit-btn"
+                        type="submit">Submit
+                    </button>
+                    </div>
+                    <br />
+                    <div className="horizontal"></div>
+                    <br />
+                </div>
 
             </div>
         </div>
