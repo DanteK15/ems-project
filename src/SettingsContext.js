75cc543c1186a3b52f9546fa3d8a8a9d3804dafa
Brandon Tishendorf <Tishendorfb@gmail.com>
2020-10-12 18:49:55 -0700

    Update SettingsContext.js

---
+++ src/SettingsContext.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+//Create an object called SettingsContext
+//We can store any data we want in this object
+//and it will become effectively global data
+//because we can call this context from any
+//component level
+const SettingsContext = React.createContext()
+
+
+
+export default SettingsContext
