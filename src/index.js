7dc7e7f9411a35a54c7d39de393f9c2006fb952a
Brandon Tishendorf <Tishendorfb@gmail.com>
2020-10-12 17:00:30 -0700

    Created Context File, wrote example code in index.js
    Wrote examples of how to use the context API, and created the js file for the context. I need to see the settings file before I can implement the functionality of this code.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import SettingsContext from './SettingsContext'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +12,36 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+//This is the format for creating the context for the
+//settings portion of the JS code, this code will
+//be placed where the settings and input code is
+//to create a context object that will make
+//those values global
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      locale: 'en'
+    }
+  }
+  render() {
+    return (
+      <SettingsContext.Provider value = {this.state.locale}>
+        <Home />
+      </SettingsContext.Provider>
+    )
+    }
+  }
+}
+
+//This is example code for accessing the settings data
+//via the context API
+//<SettingsConext.Consumer>
+//  {(locale) => <Posts locale={locale} />}
+//</SettingsConext.Consumer>
+
+//
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
