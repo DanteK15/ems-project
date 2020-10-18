46c92805277b60274644e65ec37955760d8aa9a0
Jason Zhang <43938722+Zhi22@users.noreply.github.com>
2020-10-17 18:36:51 -0700

    Update Results.js

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -7,7 +7,7 @@ import {withScriptjs, withGoogleMap, GoogleMap, DirectionsRenderer, Polyline, Ma
 
 const MapWithADirectionsRenderer = compose(
     withProps({
-        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAlfhEWVHzGhLdO2tT4hFUFLGT2pjLViho&v=3.exp&libraries=geometry,drawing,places",
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
         loadingElement: <div style={{ height: `100%` }} />,
         containerElement: <div style={{ height: `400px` }} />,
         mapElement: <div style={{ height: `100%` }} />,
