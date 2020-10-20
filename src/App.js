0dcdfa0deb98a512869abfc4b890401d60a2da31
angiemcgraw <mangie@pdx.edu>
2020-10-19 22:17:05 -0700

    input page setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Info from './components/Info/Info';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Settings from './components/Settings/Settings'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+          <Route exact path = "/">
+            <Info />
+          </Route>
+          <Route path="/settings">
+            <Settings />
+          </Route>
+        </Router>
     </div>
   );
 }
