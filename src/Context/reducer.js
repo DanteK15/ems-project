670832099bfa7c59a913f853e3d3adad4dd47785
Jordan Co <cojor@pdx.edu>
2020-11-01 09:51:37 -0800

    added testing for add hopsitals

--- src/Context/reducer.js
+++ src/Context/reducer.js
@@ -1,22 +1,26 @@
 // initial state is an object
-// data lives in here, the term will change
+// TODO: Try changing this to a function that will first load from Local Storage, then return state
 export const initialState = {
-    hospitals: [],
-    helicopters: [],
-    gmaps: {},
-    patientLocal: {},
-    destination: {}
+    hospitals: [],      // List of saved hospital Places Objects
+    helicopters: [],    // List of saved helicopter Places Objects
+    gmaps: {},          // Gmaps instance, contains Map instance and Map object   
+    patientLocal: {},   // Patient location places object <-- TODO: consider changing just lat/lng object
+    destination: {},    // Destination hosptial places object
+    heliOrigin: {}      // Helicopter origin places object
 
+    // Will need patientLocal, destination, heliOrigin for calculation
 }
 
 // whenever we change the data layer, we dispatch an action
-// that changes/sets the serch term
+// TODO: Add delete actions for Helicopter & Hospitals
+// TODO: Add clear action to reset patientLocal, destination, heliOrigin
 export const actionTypes = {
     SET_HOSP: 'SET_HOSP',
-    SET_HELI: 'SET_HELI',
+    SET_HELIS: 'SET_HELIS',
     SET_MAPS: 'SET_MAPS',
     SET_LOC: 'SET_LOC',
     SET_DEST: 'SET_DEST',
+    SET_HELI: 'SET_HELI'
 }
 
 // state is state of data layer, action is whatever we're dispatching to 
@@ -27,7 +31,12 @@ const reducer = (state, action) => {
         case actionTypes.SET_HOSP:
             return {
                 ...state,
-                hospitals: [...action.hospitals], // <-- [...fromStorage, ...action.hospitals]
+                hospitals: [...state.hospitals, action.hospital],
+            };
+        case actionTypes.SET_HELIS:
+            return {
+                ...state,
+                helicopters: [...state.helicopters, action.helicopter],
             };
         case actionTypes.SET_MAPS:
             return {
@@ -38,6 +47,16 @@ const reducer = (state, action) => {
             return {
                 ...state,
                 patientLocal: action.patientLocal
+            }
+        case actionTypes.SET_DEST:
+            return {
+                ...state,
+                destination: action.destination
+            }
+        case actionTypes.SET_LOC:
+            return {
+                ...state,
+                heliOrigin: action.heliOrigin
             }
         default:
             return state;
