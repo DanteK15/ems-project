6a34e4608cd919b0837b32fde0442773643f7d50
rawah <rawah@pdx.edu>
2020-11-30 20:55:21 -0800

    home first in navbar

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,9 +6,9 @@ function Header() {
     return (
         <nav className="header">
             <ul>
+                <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/instructions">instructions</Link></li>
-                <li><Link to="/">Home</Link></li>
             </ul>
         </nav>
     )
