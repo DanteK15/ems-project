77da2c413d5f37753c8fd7ef0e058817386e7ace
Yufei <46723525+YufeiT@users.noreply.github.com>
2020-12-02 00:04:22 -0800

    Redesign nav bar

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,10 +5,11 @@ import {Link} from 'react-router-dom';
 function Header() {
     return (
         <nav className="header">
+            <a class="nav-brand" href="/">EMS Calculator</a>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
-                <li><Link to="/instructions">instructions</Link></li>
+                <li><Link to="/instructions">Instructions</Link></li>
             </ul>
         </nav>
     )
